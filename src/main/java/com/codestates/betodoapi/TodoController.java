500a01a185e2db2bb19193aad919e9bf2a536059
jmkim0 <jmkim000000@gmail.com>
2022-12-13 01:43:42 +0900

    URL 주입 방식 변경

--- src/main/java/com/codestates/betodoapi/TodoController.java
+++ src/main/java/com/codestates/betodoapi/TodoController.java
@@ -5,33 +5,33 @@ import com.codestates.betodoapi.dto.TodoPostDto;
 import com.codestates.betodoapi.dto.TodoResponseDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
 @Validated
 @CrossOrigin
 @RestController
+@AllArgsConstructor
 public class TodoController {
     private final TodoService todoService;
     private final TodoMapper mapper;
-    private final String rootUrl;
-
-    public TodoController(TodoService todoService, TodoMapper mapper, @Value("${root-url:/}") String rootUrl) {
-        this.todoService = todoService;
-        this.mapper = mapper;
-        this.rootUrl = rootUrl;
-    }
 
     @PostMapping("/")
     public ResponseEntity<TodoResponseDto> postTodo(@Valid @RequestBody TodoPostDto postDto) {
         Todo todo = todoService.createTodo(mapper.todoPostDtoToTodo(postDto));
 
-        return new ResponseEntity<>(mapper.todoToTodoResponseDto(todo, rootUrl + todo.getId()),
+        return new ResponseEntity<>(
+                mapper.todoToTodoResponseDto(
+                        todo,
+                        ServletUriComponentsBuilder.fromCurrentRequest()
+                                .scheme("https").path(todo.getId().toString()).toUriString()
+                ),
                 HttpStatus.CREATED
         );
     }
@@ -39,7 +39,10 @@ public class TodoController {
     @GetMapping("/")
     public ResponseEntity<List<TodoResponseDto>> getTodos() {
         return new ResponseEntity<>(
-                mapper.todosToTodoResponseDtos(todoService.readTodos(), rootUrl),
+                mapper.todosToTodoResponseDtos(
+                        todoService.readTodos(),
+                        ServletUriComponentsBuilder.fromCurrentRequest().scheme("https").toUriString()
+                ),
                 HttpStatus.OK
         );
     }
@@ -47,7 +50,10 @@ public class TodoController {
     @GetMapping("/{id}")
     public ResponseEntity<TodoResponseDto> getTodo(@PathVariable @Positive Long id) {
         return new ResponseEntity<>(
-                mapper.todoToTodoResponseDto(todoService.readTodo(id), rootUrl + id),
+                mapper.todoToTodoResponseDto(
+                        todoService.readTodo(id),
+                        ServletUriComponentsBuilder.fromCurrentRequest().scheme("https").toUriString()
+                ),
                 HttpStatus.OK
         );
     }
@@ -58,7 +64,7 @@ public class TodoController {
         return new ResponseEntity<>(
                 mapper.todoToTodoResponseDto(
                         todoService.updateTodo(mapper.todoPatchDtoToTodo(id, patchDto)),
-                        rootUrl + id
+                        ServletUriComponentsBuilder.fromCurrentRequest().scheme("https").toUriString()
                 ),
                 HttpStatus.OK
         );
