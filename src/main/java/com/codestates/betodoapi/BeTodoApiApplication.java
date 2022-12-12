7f7eedecbed6be946a720668350108b87d19893c
jmkim0 <jmkim000000@gmail.com>
2022-12-12 11:37:06 +0900

    First commit

---
+++ src/main/java/com/codestates/betodoapi/BeTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.betodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTodoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/betodoapi/BeTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.betodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
